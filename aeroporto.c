da47ce53e792c5fc189774363afac9c997de8410
guiamado <amado.guigui@gmail.com>
2018-04-19 16:19:04 -0300

    [ADD] Adicionando função para inserção na fila #1

--- aeroporto.c
+++ aeroporto.c
@@ -16,15 +16,17 @@ struct fila {
 typedef struct fila Fila;
 
 Fila* cria_fila();
+Node* insere_fim (Node* fim, int v);
+void insere (Fila* f, int v);
 int geraRand(int min, int max);
 
 int main ()
 {
 	int teste;
 
-	teste = geraRand(0,3);
+	//teste = geraRand(0,3);
 	
-	printf("%d", &teste);
+	//printf("%d", &teste);
 
 	return 0;
 }
@@ -37,6 +39,27 @@ Fila* cria_fila()
 
 	return fila;
 
+}
+
+Node* insere_fim (Node* fim, int v)
+{
+	Node* no = (Node*) malloc(sizeof(Node));
+
+	no->info = v;
+	no->prox = NULL;
+
+	if(fim != NULL)
+		fim->prox = no;
+	
+	return no;
+}
+
+void insere (Fila* f, int v)
+{
+	f->fim = insere_fim(f->fim, v);
+
+	if(f->inicio == NULL)
+		f->inicio = f->fim;
 }
 
 int geraRand(int min, int max)
